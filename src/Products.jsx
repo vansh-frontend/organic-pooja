32c7a8d8b6833242a87afbea94864c77705afbf3
vansh-frontend <vanshdhalor04@gmail.com>
2024-08-29 15:00:48 +0530

    init

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,11 +1,68 @@
-// import React from 'react'
+// Products.js
+
+import './Products.css'; // Import the CSS file for styling
+
+const products = [
+  {
+    id: 1,
+    name: 'Product 1',
+    image: 'img/pro1.jpg',
+    price: '$99.99',
+  },
+  {
+    id: 2,
+    name: 'Product 2',
+    image: 'img/pro2.jpg',
+    price: '$149.99',
+  },
+
+  {
+    id: 3,
+    name: 'Product 2',
+    image: 'img/pro3.jpg',
+    price: '$149.99',
+  },
+
+  {
+    id: 4,
+    name: 'Product 2',
+    image: 'img/pro4.jpg',
+    price: '$149.99',
+  },
+
+
+  {
+    id: 5,
+    name: 'Product 2',
+    image: 'img/pro5.jpg',
+    price: '$149.99',
+  },
+  
+];
 
 const Products = () => {
   return (
-    <div>
-      Products
+    <div className="products-container">
+      {products.map(product => (
+        <div key={product.id} className="product-card">
+          <div className="product-image-container">
+            <img src={product.image} alt={product.name} className="product-image" />
+          </div>
+          <div className="product-info">
+            <h3 className="product-name">{product.name}</h3>
+            <p className="product-price">{product.price}</p>
+          </div>
+          <div className="overlay">
+            <div className="overlay-content">
+              <h3 className="overlay-title">{product.name}</h3>
+              <p className="overlay-price">{product.price}</p>
+              <button className="overlay-button">View Details</button>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
