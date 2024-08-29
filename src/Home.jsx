8b247ecf2b8353683362714f52a718e84067eb7e
vansh-frontend <vanshdhalor04@gmail.com>
2024-08-29 19:12:37 +0530

    first

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,10 @@
-
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // Import the carousel styles
 import './Home.css'; // Custom styles for additional customization if needed
 
 const Home = () => {
   return (
-    <div className="w-full">
+    <div className="w-full overflow-hidden rounded-lg">
       <DemoCarousel />
     </div>
   );
@@ -20,23 +19,23 @@ const DemoCarousel = () => {
       infiniteLoop
       autoPlay
       transitionTime={500}
-      interval={3000}
+      interval={2000}
       className="w-full"
     >
-      <div className="relative w-full h-64 sm:h-80 md:h-96 lg:h-[32rem]">
-        <img src="img/pro1.jpg" alt="Slide 1" className="object-cover w-full h-full" />
+      <div className="relative w-full h-64 sm:h-80 md:h-96 lg:h-[32rem] rounded-lg overflow-hidden">
+        <img src="img/pro1.jpg" alt="Slide 1" className="object-cover w-full h-full rounded-lg" />
         <p className="absolute px-4 py-2 text-xs text-white bg-black bg-opacity-50 rounded-lg bottom-4 left-4 sm:text-sm md:text-base lg:text-lg">
           Legend 1
         </p>
       </div>
-      <div className="relative w-full h-64 sm:h-80 md:h-96 lg:h-[32rem]">
-        <img src="img/pro2.jpg" alt="Slide 2" className="object-cover w-full h-full" />
+      <div className="relative w-full h-64 sm:h-80 md:h-96 lg:h-[32rem] rounded-lg overflow-hidden">
+        <img src="img/pro2.jpg" alt="Slide 2" className="object-cover w-full h-full rounded-lg" />
         <p className="absolute px-4 py-2 text-xs text-white bg-black bg-opacity-50 rounded-lg bottom-4 left-4 sm:text-sm md:text-base lg:text-lg">
           Legend 2
         </p>
       </div>
-      <div className="relative w-full h-64 sm:h-80 md:h-96 lg:h-[32rem]">
-        <img src="img/pro3.jpg" alt="Slide 3" className="object-cover w-full h-full" />
+      <div className="relative w-full h-64 sm:h-80 md:h-96 lg:h-[32rem] rounded-lg overflow-hidden">
+        <img src="img/pro3.jpg" alt="Slide 3" className="object-cover w-full h-full rounded-lg" />
         <p className="absolute px-4 py-2 text-xs text-white bg-black bg-opacity-50 rounded-lg bottom-4 left-4 sm:text-sm md:text-base lg:text-lg">
           Legend 3
         </p>
