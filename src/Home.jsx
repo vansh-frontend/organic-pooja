83918442a9b4b336b1b1c67b668e248dddc5a2a9
vansh-frontend <vanshdhalor04@gmail.com>
2024-09-06 14:16:30 +0530

    first

--- src/Home.jsx
+++ src/Home.jsx
@@ -45,7 +45,7 @@ const Home = () => {
   {products.map((product, index) => (
     <div
       key={index}
-      className="relative flex flex-col sm:flex-row overflow-hidden bg-white rounded-lg shadow-lg mx-4 sm:mx-6 md:mx-8 lg:mx-12 xl:mx-16 h-auto sm:h-72 md:h-80 lg:h-96 xl:h-[500px]"
+      className="relative flex flex-col sm:flex-row overflow-hidden bg-white rounded-lg shadow-lg mx-2 sm:mx-4 md:mx-6 lg:mx-8 xl:mx-12 h-auto sm:h-72 md:h-80 lg:h-96 xl:h-[500px]"
     >
       <Link to={product.link} className="w-full h-auto sm:w-1/2">
         <img
@@ -55,27 +55,25 @@ const Home = () => {
         />
       </Link>
       <div
-        className="flex flex-col items-center justify-center w-full h-full p-4 sm:w-1/2 sm:p-6 md:p-8 lg:p-10"
+        className="flex flex-col items-start justify-center w-full h-full p-4 sm:w-1/2 sm:p-6 md:p-8 lg:p-10"
         style={{ backgroundColor: '#D0F0C0' }} // Peach Light Green Color
       >
-        <div className="text-center">
+        <div className="text-left sm:text-center">
           <h3 className="text-lg font-bold text-gray-800 sm:text-xl md:text-2xl">{product.title}</h3>
           <p className="mt-2 text-xs sm:text-sm md:text-base lg:text-lg">
             Organic Pooja offers a premium beauty experience using only organic products. From hair styling to nail treatments and makeup, we specialize in creating contemporary and timeless looks for every occasion. Our mission is to make you shine like a star naturally.
           </p>
-          <div className="flex justify-center mt-4">
+          <div className="flex justify-start mt-4 sm:justify-center">
             <button className="px-4 py-2 font-serif text-sm text-white transition duration-300 transform bg-black rounded-full sm:px-6 sm:py-3 sm:text-base hover:scale-105 hover:bg-gray-700 hover:shadow-lg focus:outline-none">
               SHOP NOW
             </button>
           </div>
         </div>
       </div>
-      {/* <div className="absolute p-2 bg-white rounded-lg shadow-lg bottom-4 right-4 sm:bottom-6 sm:right-6">
-        <p className="m-0 text-xs font-semibold text-gray-800 sm:text-sm">Organic By Pooja</p>
-      </div> */}
     </div>
   ))}
 </Carousel>
+
 
 
       {/* Centered Content - Services Section */}
