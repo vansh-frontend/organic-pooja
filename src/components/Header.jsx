3ef132838f320aa6aa03afccba23c4d35ac9fd28
vansh-frontend <vanshdhalor04@gmail.com>
2024-09-17 17:40:43 +0530

    init

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,12 +7,12 @@ const Header = () => {
     <header className="sticky top-0 z-50 shadow-lg bg-gradient-to-r from-gray-50 via-gray-100 to-gray-200">
       {/* Call Us Section */}
       <div className="flex items-center justify-center py-2 space-x-2 text-white bg-black">
-        <IconPhoneCall size={20} className="text-yellow-400" />
-        <p className="text-sm font-medium">
-          Call us at:
-          <a href="tel:+918171924503" className="font-semibold text-yellow-300 hover:underline"> +91 8171924503</a>
-        </p>
-      </div>
+  <IconPhoneCall size={20} className="text-yellow-400" />
+  <p className="text-sm font-medium">
+    Call us at:
+    <a href="tel:+918171924503" className="font-semibold text-yellow-300 hover:underline"> +91 8171924503</a>
+  </p>
+</div>
 
       {/* Main Header */}
       <div className="flex items-center justify-between p-2 border-b border-gray-300 lg:px-4 lg:py-1">
@@ -22,18 +22,19 @@ const Header = () => {
         </div>
 
         {/* Centered Logo and Title */}
-        <div className="flex justify-center flex-1">
-          <NavLink to="/" className="flex items-center space-x-2">
-            <img 
-              src="img/pooja 2.png"
-              alt="Logo"
-              className="object-cover w-12 h-12 rounded-full shadow-lg"
-            />
-            <h1 className="text-lg md:text-xl lg:text-2xl font-bold tracking-widest text-gray-800 uppercase font-['Roboto_Slab'] ml-4">
-              Organic <span className="font-extrabold text-yellow-500">Pooja</span>
-            </h1>
-          </NavLink>
-        </div>
+        <div className="flex justify-center mt-4 md:justify-start md:mt-0 md:ml-8 lg:ml-28">
+  <NavLink to="/" className="flex items-center space-x-2">
+    <img 
+      src="img/pooja 2.png"
+      alt="Logo"
+      className="object-cover w-10 h-10 rounded-full shadow-lg md:w-12 md:h-12"
+    />
+    <h1 className="text-xl md:text-2xl lg:text-3xl font-bold tracking-widest text-gray-800 uppercase font-['Roboto_Slab'] ml-2 md:ml-4">
+      Organic <span className="font-extrabold text-yellow-500">Pooja</span>
+    </h1>
+  </NavLink>
+</div>  
+
 
         {/* Navbar for Desktop (Right Items) */}
         <div className="hidden lg:flex lg:space-x-4">
