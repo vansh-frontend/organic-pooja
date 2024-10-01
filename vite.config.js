ac9cfac7d763f4df2d54cb60537af7db1bdfc846
vansh-frontend <vanshdhalor04@gmail.com>
2024-10-01 10:36:06 +0530

    init

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    chunkSizeWarningLimit: 5000, 
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        }
-      }
-    }
-  }
-});
+  plugins: [react()],
+})
